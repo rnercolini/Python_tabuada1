167301396487e4f95dbeecf642eefc97f5f89b71
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 14:29:49 -0300

    Add files via upload

---
+++ Desafio9.py
@@ -0,0 +1,29 @@
+# Calcula a tabuada de um número
+n = int(input('Digite um número para calcular a sua tabuada: '))
+n1 = n * 1
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+n7 = n * 7
+n8 = n * 8
+n9 = n * 9
+n10 = n * 10
+print('A tabuada de {0} é'
+      ':\n1 x {0} = {1}\n2 x {0} = {2}\n3 x {0} = {3}\n4 x {0} = {4}\n5 x {0} = {5}'.format(n, n1, n2, n3, n4, n5))
+print('6 x {0} = {1}\n7 x {0} = {2}\n8 x {0} = {3}\n9 x {0} = {4}\n10 x {0} = {5}'.format(n, n6, n7, n8, n9, n10))
+print('=' * 20)
+print('Outra forma de fazer')
+print('=' * 20)
+print('{0} x {1:2} = {2}'.format(n, 1, (n * 1)))
+print('{0} x {1:2} = {2}'.format(n, 2, (n * 2)))
+print('{0} x {1:2} = {2}'.format(n, 3, (n * 3)))
+print('{0} x {1:2} = {2}'.format(n, 4, (n * 4)))
+print('{0} x {1:2} = {2}'.format(n, 5, (n * 5)))
+print('{0} x {1:2} = {2}'.format(n, 6, (n * 6)))
+print('{0} x {1:2} = {2}'.format(n, 7, (n * 7)))
+print('{0} x {1:2} = {2}'.format(n, 8, (n * 8)))
+print('{0} x {1:2} = {2}'.format(n, 9, (n * 9)))
+print('{0} x {1:2} = {2}'.format(n, 10, (n * 10)))
+print('=' * 20)
